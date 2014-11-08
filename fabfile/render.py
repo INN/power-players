c444241f8643f535e7a435df5cbf45bfb38c806c
Ryan Nagle <rmnagle@gmail.com>
2014-11-08 09:33:54 -0600

    we don't need to determine which module a view function is in anymore

--- fabfile/render.py
+++ fabfile/render.py
@@ -105,14 +105,6 @@ def render_all():
             g.compile_includes = True
             g.compiled_includes = compiled_includes
 
-            bits = name.split('.')
-
-            # Determine which module the view resides in
-            if len(bits) > 1:
-                module, name = bits
-            else:
-                module = 'app'
-
             view = app.app.view_functions[name]
             content = view()
 
